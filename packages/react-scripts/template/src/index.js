dac25f6c42a8053d58a90fa11dceb690e7113e60
Josh Brunner <josh@helpfulhuman.com>
2017-03-10 10:50:01 -0800

    Added a routes.js file

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { browserHistory, Router } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { browserHistory, Router, Route, IndexRoute } from 'react-router';
-
 import createStore from './Services/Store';
-
-import App from './App';
-import Landing from './Containers/Landing';
 
 const store = createStore();
 const history = syncHistoryWithStore(browserHistory, store);
@@ -15,10 +11,7 @@ const history = syncHistoryWithStore(browserHistory, store);
 render(
   <Provider store={store}>
     <Router history={history}>
-      <Route path='/' component={App}>
-        <IndexRoute component={Landing} />
-        {/*<Route path='somepage' component={SomePage} />*/}
-      </Route>
+      {routes}
     </Router>
   </Provider>
   , document.getElementById('root')
