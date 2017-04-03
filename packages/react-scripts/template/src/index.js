12fae5a4d9ea4d8e9eda51df15951eb49d033ec2
Ryan Canfield <ryancanfield@me.com>
2017-04-03 09:17:43 -0700

    Add react-aXe

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { browserHistory, Router } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import createStore from './Services/Store';
 import routes from './routes';
+import axe from 'react-axe';
 
 const store = createStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
-render(
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
+
+ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       {routes}
     </Router>
-  </Provider>
-  , document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
