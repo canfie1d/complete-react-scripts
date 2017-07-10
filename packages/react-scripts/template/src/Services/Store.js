d5e7358996906b78ebc42ca08e00e6f35235fe53
Ryan Canfield <ryancanfield@me.com>
2017-07-10 16:15:21 -0700

    Update packages to remove console warnings

--- packages/react-scripts/template/src/Services/Store.js
+++ packages/react-scripts/template/src/Services/Store.js
@@ -1,20 +1,17 @@
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { browserHistory } from 'react-router';
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import reducers from '../Reducers';
 
-let middleware = [ thunk, routerMiddleware(browserHistory) ];
+let middleware = [thunk, routerMiddleware(browserHistory)];
 
 if (process.env.NODE_ENV !== 'production') {
   const logger = createLogger({ collapsed: true });
-  middleware = [ ...middleware, logger ];
+  middleware = [...middleware, logger];
 }
 
-export default function () {
-  return createStore(
-    reducers,
-    applyMiddleware(...middleware)
-  );
+export default function() {
+  return createStore(reducers, applyMiddleware(...middleware));
 }
