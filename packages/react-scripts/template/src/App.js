2409a4e2d34b41fc7b1276d286c6e2dea16262cc
Ryan Canfield <ryancanfield@me.com>
2017-08-15 14:10:02 -0700

    Move console log

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -3,10 +3,6 @@ import './App.styl';
 
 export default class App extends React.Component {
   render() {
-    if (process.env.NODE_ENV !== 'production') {
-      console.log('👨‍🚀 Application properties:\n', this.props);
-    }
-
     return (
       <div className="app">
         <div className="app__container">
--- packages/react-scripts/template/src/Components/Landing.js
+++ packages/react-scripts/template/src/Components/Landing.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 export default class Landing extends React.Component {
   render() {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('👨‍🚀 Application properties:\n', this.props);
+    }
+
     return (
       <div className="landing">
         <div className="outerspace">
           <span className="spacheman" role="img" aria-label="spaceman">
             👨‍🚀
           </span>
-          <span className="rocket" role="img" aria-label="rocket">🚀</span>
+          <span className="rocket" role="img" aria-label="rocket">
+            🚀
+          </span>
         </div>
         <div>
           <h1>Welcome to Helpful Human's React template.</h1>
@@ -20,7 +26,9 @@ export default class Landing extends React.Component {
         <button onClick={this.props.addNumber}>Add</button>
         <br />
         <br />
-        <span>value: {this.props.number}</span>
+        <span>
+          value: {this.props.number}
+        </span>
       </div>
     );
   }
