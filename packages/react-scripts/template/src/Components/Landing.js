0ea4fff743cbe99dd04f7007104ab1c258ac61e3
Ryan Canfield <ryancanfield@me.com>
2017-11-01 16:27:35 -0700

    Update favicon, color palette and emojis

--- packages/react-scripts/template/src/Components/Landing.js
+++ packages/react-scripts/template/src/Components/Landing.js
@@ -3,32 +3,24 @@ import React from 'react';
 export default class Landing extends React.Component {
   render() {
     if (process.env.NODE_ENV !== 'production') {
-      console.log('👨‍🚀 Application properties:\n', this.props);
+      console.log('👨‍🔬 Application properties:\n', this.props);
     }
 
     return (
       <div className="landing">
-        <div className="outerspace">
-          <span className="spacheman" role="img" aria-label="spaceman">
-            👨‍🚀
-          </span>
-          <span className="rocket" role="img" aria-label="rocket">
-            🚀
-          </span>
-        </div>
+        <span className="human" role="img" aria-label="scientist">
+          👨‍🔬
+        </span>
         <div>
           <h1>Welcome to Helpful Human's React template.</h1>
           <h2>Check the console for application properties.</h2>
         </div>
         <h3>Demo Redux store</h3>
-        <br />
-        <button onClick={this.props.subtractNumber}>Subtract</button>
-        <button onClick={this.props.addNumber}>Add</button>
-        <br />
-        <br />
-        <span>
-          value: {this.props.number}
-        </span>
+        <div>
+          <button onClick={this.props.subtractNumber}>Subtract</button>
+          <button onClick={this.props.addNumber}>Add</button>
+          <div>value: {this.props.number}</div>
+        </div>
       </div>
     );
   }
