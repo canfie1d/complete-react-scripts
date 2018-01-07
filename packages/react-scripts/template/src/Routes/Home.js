72174de9cf959477390ff94e046103876dc646c3
Ryan <ryancanfield@me.com>
2018-01-07 13:26:21 -0800

    Updates to template files

--- packages/react-scripts/template/src/Routes/Home.js
+++ packages/react-scripts/template/src/Routes/Home.js
@@ -3,17 +3,19 @@ import React from 'react';
 export default class Landing extends React.Component {
   render() {
     if (process.env.NODE_ENV !== 'production') {
-      console.log('👨‍🔬 Application properties:\n', this.props);
+      console.log('Application properties:\n', this.props);
     }
 
     return (
       <div className="landing">
         <span className="human" role="img" aria-label="scientist">
-          👨‍🔬
+          🏋🏼‍
         </span>
         <div>
-          <h1>Welcome to Helpful Human's React template.</h1>
+          <h1>Welcome to your Complete React template.</h1>
           <h2>Check the console for application properties.</h2>
+          <h2>~or~</h2>
+          <h2>Edit Home.js to get started.</h2>
         </div>
         <h3>Demo Redux store</h3>
         <div>
--- packages/react-scripts/template/src/routes.js
+++ packages/react-scripts/template/src/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 
 // PAGES
-import Landing from './Containers/Landing';
+import Home from './Containers/Home';
 import NotFound from './Components/NotFound';
 
 export default (
   <Switch>
-    <Route exact path="/" component={Landing} />
+    <Route exact path="/" component={Home} />
     {/*<Route path='/<path-name>' component={Component} />*/}
     <Route component={NotFound} />
   </Switch>
