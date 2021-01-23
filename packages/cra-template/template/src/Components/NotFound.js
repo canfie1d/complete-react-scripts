6bcfcaa26d8a09652a4bfc15cb6533d56b5f9f51
Ryan Canfield <ryancanfield@me.com>
2021-01-22 22:51:09 -0800

    Update react import

--- packages/cra-template/template/src/Components/NotFound.js
+++ packages/cra-template/template/src/Components/NotFound.js
@@ -1,13 +1,11 @@
-import React from 'react';
+const NotFound = props => {
+  return (
+    <div>
+      <h1>404 </h1>
+      <h2>Page Not Found</h2>
+      <p>Sorry, but the page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
-export default class NotFound extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>404 </h1>
-        <h2>Page Not Found</h2>
-        <p>Sorry, but the page you are looking for does not exist.</p>
-      </div>
-    );
-  }
-}
+export default NotFound;
